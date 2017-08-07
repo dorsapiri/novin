55b936c0cc613de727d1d1d3e54dfc71f20f0265
parsaasoodeh <p.asoodeh24@gmail.com>
2017-08-07 16:23:42 +0430

    add picture

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,6 @@
 /**
  * Created by dorsa on 7/17/17.
  */
-$('.carousel').carousel({
-    interval: 3000 //changes the speed
-})
+// $('.carousel').carousel({
+//     interval: 30000//changes the speed
+// });
